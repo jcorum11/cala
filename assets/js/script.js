9cd3fc9e226b85645ec711a8e91b29f901355d01
Jacob Corum <cor15031@byui.edu>
2020-05-30 11:47:57 -0600

    momentjs to each timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
+// set date in header
+var setDate = function() {
+    var currentDay = moment().format("LL LT");
+    $("#currentDay").text(currentDay);
+}
 
-var currentDay = moment().format("LL LT");
-$("#currentDay").text(currentDay)
-console.log(currentDay);
+
+// put time in time blocks
+var setTimeBlocks = function() {
+    var beginningOfDay = moment("8:00", "h:ss");
+    $(".container").find(".time-block").each(function() {
+        beginningOfDay.add(1, "hour");
+        $(this).text(beginningOfDay.format("LT"));
+    })
+}
+
+
+setDate();
+setTimeBlocks();
