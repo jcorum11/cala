31d973a402516f4c177ec6942a7e838ead3fc568
Jacob Corum <cor15031@byui.edu>
2020-05-30 13:06:01 -0600

    revised html classes, reverted css time-block attribute, js class tense function, js timeList array, revised js comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,38 @@ var setDate = function() {
     $("#currentDay").text(currentDay);
 }
 
-// put time in time blocks
+// put time in time blocks beginning at 9:00am and ending at 5:00pm
 var setTimeBlocks = function() {
+    // set beginning to 8:00 am so we can add 1 and start at 9:00am
     var beginningOfDay = moment("8:00", "h:ss");
-    $(".container").find(".time-block").each(function() {
+    $(".container").find(".hour").each(function() {
         beginningOfDay.add(1, "hour");
+        // add time to DOM .time-blocks
         $(this).text(beginningOfDay.format("LT"));
     })
 }
 
+// add class tense
+var addClassTense = function(tense) {
+    $(".container").find(".row > div:nth-child(2)").each(function() {
+        $(this).addClass(tense);
+    })
+}
+
+
+
+
+// if blocks are in the past then 
+// make them grey
+// if the current time is in a block then
+// make that block red
+// if blocks are in the future then 
+// make that block green
+
 setDate();
 setTimeBlocks();
+
+// check if blocks are in the past
+var timeList = $(".container").find(".hour").each(function() {
+    console.log($(this).text() );
+})
